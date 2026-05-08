f991ff176bb58a9bb86fdaa05245fd613543ed11
Newton COB Junior <newtonjr@gmail.com>
2026-05-08 11:51:55 -0300

    feat: [tarefa/02-records] criacao de estruturas

---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Main.java
@@ -0,0 +1,28 @@
+package br.com.zenon.fraud;
+
+import java.math.BigDecimal;
+
+public class Main {
+    static void main(String[] args) {
+        Transaction transacao01 = new Transaction(
+                1,
+                TransactionType.PAYMENT,
+                new BigDecimal("9839.64"),
+                new TransactionCustomer("C1231006815", new BigDecimal("170136.0"), new BigDecimal("160296.36")),
+                new TransactionCustomer("M1979787155", new BigDecimal("0.0"), new BigDecimal("0.0")),
+                false,
+                false);
+
+        Transaction transacao02 = new Transaction(
+                743,
+                TransactionType.CASH_OUT,
+                new BigDecimal("850002.52"),
+                new TransactionCustomer("C1280323807", new BigDecimal("850002.52"), new BigDecimal("0.0")),
+                new TransactionCustomer("C873221189", new BigDecimal("6510099.11"), new BigDecimal("7360101.63")),
+                true,
+                false);
+
+        IO.println(transacao01);
+        IO.println(transacao02);
+    }
+}
---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/TransactionCustomer.java
@@ -0,0 +1,8 @@
+package br.com.zenon.fraud;
+
+import java.math.BigDecimal;
+
+public record TransactionCustomer(String name,
+                                  BigDecimal oldbalance,
+                                  BigDecimal newbalance) {
+}
