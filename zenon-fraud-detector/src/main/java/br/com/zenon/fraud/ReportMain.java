1d1116a84f47fc2dcd23383b9ea9845ba3366358
Newton COB Junior <newtonjr@gmail.com>
2026-05-10 12:49:56 -0300

    feat: [tarefa/08-i18n] testando a mudança de idioma

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/ReportMain.java
@@ -1,13 +1,45 @@
 package br.com.zenon.fraud;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class ReportMain {
-    void main() {
+    void main(String[] args) {
+        // en-US | pt-BR
+        String language = (args.length > 0 ? args[0]: "pt-BR");
+        var locale = Locale.forLanguageTag(language);
+
+        var integerFormatter = NumberFormat.getIntegerInstance(locale);
+        var currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+        currencyFormatter.setCurrency(Currency.getInstance(locale));
+
+        // determina qual arquivo do idioma vai carregar do resorcou usando o locale
+        var resourceBundle = ResourceBundle.getBundle("report", locale);
+
         var transactionReport = new TransactionReport();
         StatisticsTransaction statistics = transactionReport.generateReport("data/PS_20174392719_1491204439457_log.csv");
+
+        // formata valores numéricos
+        String fmtTotalTransactions = integerFormatter.format(statistics.totalTransactions());
+        String fmtTotalFrauds = integerFormatter.format(statistics.totalFrauds());
+        String fmtTotalAmount = currencyFormatter.format(statistics.totalAmount());
+
+        // extrai os textos do resource
+        String msgTotalTransactions = resourceBundle.getString("label.total.transactions");
+        String msgTotalFrauds = resourceBundle.getString("label.total.frauds");
+        String msgTotalAmount = resourceBundle.getString("label.total.amount");
+
         IO.println("""
-        Total de linhas: %d
-        Total de fraudes: %d
-        Valor total transacionado: %.2f
-        """.formatted(statistics.totalTransactions(), statistics.totalFrauds(), statistics.totalAmount()));
+        %s: %s
+        %s: %s
+        %s: %s
+        """.formatted(
+                msgTotalTransactions, fmtTotalTransactions,
+                msgTotalFrauds, fmtTotalFrauds,
+                msgTotalAmount, fmtTotalAmount
+        ));
     }
 }
